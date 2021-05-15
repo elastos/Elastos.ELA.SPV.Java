981abcfe5495313fb9aab9b38642f3f0f59a1351
lifayi <lifayi2008@163.com>
2021-05-15 15:06:28 +0800

    change for mavenCentral release

--- app/src/main/jni/IDChainSubWallet.cpp
+++ app/src/main/jni/IDChainSubWallet.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "Utils.h"
-#include "IIDChainSubWallet.h"
+#include "IIdChainSubWallet.h"
 #include "nlohmann/json.hpp"
 
 using namespace Elastos::ElaWallet;
